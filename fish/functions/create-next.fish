857a1d2faf0de1a60b0c8b37bb5cf219041fcf80
Xanazf <hotdamnsucka@gmail.com>
2024-03-18 02:49:23 +0200

    base dotfiles

---
+++ fish/functions/create-next.fish
@@ -0,0 +1,3 @@
+function create-next
+    npx create-next-app@latest $argv --ts --tailwind --eslint --app
+end
---
+++ fish/functions/create_next.fish
@@ -0,0 +1,3 @@
+function create_next
+ npx create-next-app@latest $argv[1] --ts --tailwind --eslint --app; 
+end
