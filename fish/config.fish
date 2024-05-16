22eb7006dd1d55c9777066053d478db53d48431b
Xanazf <hotdamnsucka@gmail.com>
2024-05-16 14:40:16 +0300

    new commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,23 @@
-neofetch
+nitch
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    echo online...
+end
+
+set -g fish_greeting
+
+
 function blur_terminal_background
-  if test $DISPLAY
-    for class in kitty konsole;
-      for ID in (xdotool search --class $class)
-        xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $ID
-      end
+    if test $DISPLAY
+        for class in kitty konsole
+            for ID in (xdotool search --class $class)
+                xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -id $ID
+            end
+        end
     end
-  end
 end
 set -x PATH /path/to/tree-sitter $PATH
 blur_terminal_background
-
-function create-next
-    npx create-next-app@latest $argv --ts --tailwind --eslint --app
-end
-funcsave create-next
